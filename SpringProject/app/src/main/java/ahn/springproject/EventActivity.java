3c1a38bfd2b4e5b201dfd17939ad2ec62645160d
AliNaqvi97 <alinaqvi1397@gmail.com>
2017-02-27 20:15:52 -0600

    Schedule App complete v2

---
+++ SpringProject/app/src/main/java/ahn/springproject/EventActivity.java
@@ -0,0 +1,93 @@
+package ahn.springproject;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class EventActivity extends AppCompatActivity {
+    ListView eventList;
+    ArrayAdapter<String> adapter;
+    List<String> events;
+    Intent intent;
+
+    Button updateButton;
+    EditText editText;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_event);
+
+        intent = getIntent();
+        String day = intent.getStringExtra("day");
+        events = new ArrayList<>();
+        eventList = (ListView) findViewById(R.id.event_list);
+
+        updateButton = (Button) findViewById(R.id.update_button);
+        editText = (EditText) findViewById(R.id.edit_text);
+
+        switch (day) {
+            case "Monday":
+                events.add("10-11AM Class");
+                events.add("12-2PM Contemplating life");
+                events.add("3-5PM Class");
+                events.add("6-7PM Attempting to do homework");
+                events.add("7-9PM Social Media");
+                events.add("9-12AM Actually doing homework but not really");
+                break;
+            case "Tuesday":
+                // TIK TOK LYRICS lmao
+                events.add("8-9AM Wake up feeling like P-Diddy");
+                events.add("9-11AM Grab glasses and go out the door ready to hit the city");
+                events.add("11AM-12PM Go back to brush teeth");
+                events.add("12PM-12AM Never come back for night");
+                break;
+            case "Wednesday":
+                events.add("10-11AM Class");
+                events.add("12-2PM Contemplating life");
+                events.add("3-5PM Class");
+                events.add("6-7PM Attempting to do homework");
+                events.add("7-9PM Social Media");
+                events.add("9-12AM Actually doing homework but not really");
+                break;
+            case "Thursday":
+                // TIK TOK LYRICS lmao
+                events.add("8-9AM Wake up feeling like P-Diddy");
+                events.add("9-11AM Grab glasses and go out the door ready to hit the city");
+                events.add("11AM-12PM Go back to brush teeth");
+                events.add("12PM-12AM Never come back for night");
+                break;
+            case "Friday":
+                events.add("10-11AM Class");
+                events.add("12-2PM Contemplating life");
+                events.add("3-5PM Class");
+                events.add("6-7PM Attempting to do homework");
+                events.add("7-9PM Social Media");
+                events.add("9-12AM Actually doing homework but not really");
+                break;
+        }
+
+        adapter = new ArrayAdapter<String>(getApplicationContext(), R.layout.app_list_item, R.id.app_name, events);
+        eventList.setAdapter(adapter);
+
+        updateButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String update = editText.getText().toString();
+                if(!update.equals(""))
+                    events.add(update);
+
+                adapter.notifyDataSetChanged();
+                editText.setText("");
+            }
+        });
+    }
+}
--- SpringProject/app/src/main/java/ahn/springproject/ScheduleActivity.java
+++ SpringProject/app/src/main/java/ahn/springproject/ScheduleActivity.java
@@ -1,13 +1,47 @@
 package ahn.springproject;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class ScheduleActivity extends AppCompatActivity {
+    ListView dayList;
+    ArrayAdapter<String> adapter;
+    List<String> days;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_schedule);
+
+        days = new ArrayList<>();
+        dayList = (ListView) findViewById(R.id.day_list);
+
+        days.add("Monday");
+        days.add("Tuesday");
+        days.add("Wednesday");
+        days.add("Thursday");
+        days.add("Friday");
+
+        adapter = new ArrayAdapter<String>(getApplicationContext(), R.layout.app_list_item, R.id.app_name, days);
+        dayList.setAdapter(adapter);
+
+        dayList.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                String day = dayList.getItemAtPosition(i).toString();
+
+                Intent intent = new Intent(getApplicationContext(), EventActivity.class);
+                intent.putExtra("day", day);
+                startActivity(intent);
+            }
+        });
     }
 }
