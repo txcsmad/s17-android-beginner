7bbc13e5c655d572a1756b5ebfc9ee61fcf713e9
AliNaqvi97 <alinaqvi1397@gmail.com>
2017-03-23 19:54:49 -0500

    Layouts lesson 1

---
+++ SpringProject/app/src/main/java/ahn/springproject/ComposeActivity.java
@@ -0,0 +1,13 @@
+package ahn.springproject;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ComposeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_compose);
+    }
+}
